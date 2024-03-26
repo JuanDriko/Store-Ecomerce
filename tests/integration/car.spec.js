5a14525c97bad0568528d4d4fef8f66d36597236
JuanDriko <juandriko@hotmail.com>
2024-03-26 00:35:57 -0500

    store-ecomerce vue3

--- tests/integration/car.spec.js
+++ tests/integration/car.spec.js
@@ -1,11 +1,11 @@
 import { mount } from '@vue/test-utils';
 import ProductsAll from '../../src/views/ProductsAll.vue';
 import NavBar from '../../src/components/NavBar.vue';
-import { useCartStore } from '../../src/store/car.js'; 
+import { useCartStore } from '../../src/store/car.js';
 
-jest.mock('../../src/store/car.js'); 
+jest.mock('../../src/store/car.js');
 
-describe('Integration Test - adicion de producto a carrito de compras', () => {
+describe('Integration Test - adición de producto a carrito de compras', () => {
   it('agregando producto a carrito reflejado en el NavBar', async () => {
     const addToCart = jest.fn();
     useCartStore.mockReturnValue({
@@ -16,23 +16,19 @@ describe('Integration Test - adicion de producto a carrito de compras', () => {
     const productsWrapper = mount(ProductsAll);
     const navBarWrapper = mount(NavBar);
 
-    await waitForComponentToRender(productsWrapper);
-    await waitForComponentToRender(navBarWrapper);
+    await productsWrapper.vm.$nextTick();
+    await navBarWrapper.vm.$nextTick();
 
     const addButton = productsWrapper.find('[data-testid="add-to-cart-button"]');
     await addButton.trigger('click');
 
-    await waitForComponentToRender(productsWrapper);
-    await waitForComponentToRender(navBarWrapper);
+    await productsWrapper.vm.$nextTick();
+    await navBarWrapper.vm.$nextTick();
 
     expect(addToCart).toHaveBeenCalled();
-    expect(addToCart.mock.calls[0][0]).toEqual(productsWrapper.vm.getProductById(1));
+    expect(addToCart.mock.calls[0][0]).toEqual(productsWrapper.vm.products[0]);
 
     const cartLength = navBarWrapper.find('.badge').text();
     expect(cartLength).toBe('1'); 
   });
 });
-
-async function waitForComponentToRender(wrapper) {
-  await wrapper.vm.$nextTick();
-}
